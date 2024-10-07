55320b27abe2275b5e9fa511f4a25605d16f4dbf
Edward Ho <pythoned77@gmail.com>
2024-10-07 21:39:07 +0800

    formed a simple RAG

--- business_logic/grant_eligibility/RAG.py
+++ business_logic/grant_eligibility/RAG.py
@@ -1,4 +1,13 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 from langchain_community.document_loaders import BSHTMLLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain.chains import RetrievalQA
+from helper_functions.llm import count_tokens,embeddings_model,llm
+
+# Document Loading
 
 file_paths = [
     "assets/grant_eligibility/family/HDB _ CPF Housing Grants for Resale Flats (Families).html",
@@ -20,3 +29,33 @@ for file in file_paths:
     except Exception as e:
         print(f"Error loading {file}: {e}")
         continue
+
+
+i = 0
+for doc in list_of_documents_loaded:
+    i += 1
+    print(f'Document {i} - "{doc.metadata.get("source")}" has {count_tokens(doc.page_content)} tokens')
+
+# In this case, we intentionally set the chunk_size to 1100 tokens, to have the smallest document (document 2) intact
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1100, chunk_overlap=10, length_function=count_tokens)
+
+# Split the documents into smaller chunks
+splitted_documents = text_splitter.split_documents(list_of_documents_loaded)
+
+# Print the number of documents after splitting
+print(f"Number of documents after splitting: {len(splitted_documents)}")
+
+# Create the vector database
+vectordb = Chroma.from_documents(
+    documents=splitted_documents,
+    embedding=embeddings_model,
+    collection_name="naive_splitter", # one database can have multiple collections
+    persist_directory="./vector_db"
+)
+
+rag_chain = RetrievalQA.from_llm(
+    retriever=vectordb.as_retriever(), llm=llm
+)
+
+llm_response = rag_chain.invoke('What is enhanced grant for family')
+print(llm_response['result'])
