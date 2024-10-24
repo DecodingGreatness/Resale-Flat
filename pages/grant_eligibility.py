711c12345f7787bbee62175ee6a59d3156e807da
Edward Ho <pythoned77@gmail.com>
2024-10-24 16:17:20 +0800

    renamed rag inside grant eligibility to grant eligibility rag

--- pages/grant_eligibility.py
+++ pages/grant_eligibility.py
@@ -3,7 +3,7 @@ import sys
 import os
 import time
 # sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
-from business_logic.grant_eligibility.rag import app
+from business_logic.grant_eligibility.grant_eligibility_rag import app
 from helper_functions.llm import security_prompt
 
 st.title("Resale Grant Eligibility Checker")
