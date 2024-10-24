34e7328fb12f6c4ef295cc570621fdb37eb34646
Edward Ho <pythoned77@gmail.com>
2024-10-24 17:22:28 +0800

    updated spinner for resale transaction price

--- pages/resale_transaction_price.py
+++ pages/resale_transaction_price.py
@@ -8,33 +8,24 @@ from helper_functions.utility import retrieve_crew_content,retrieve_crew_table
 
 st.title("Resale Transaction Price Advisor")
 
-form = st.form(key="form",clear_on_submit=True, enter_to_submit=True, border=True)
-form.subheader("Prompt")
+with st.form(key="form",clear_on_submit=True,enter_to_submit=True):
+    user_prompt = st.text_area("Enter your text here:",
+                                             value="",
+                                             height=200,
+                                             placeholder="Type something...")
+    submitted = st.form_submit_button("Submit")
+    messages = st.container(height=200)
 
-user_prompt = form.text_area("Enter your prompt here", height=200)
-messages = st.container(height=300)
 
+if submitted:
+    with st.status("Processing Input...", expanded=True) as status:
+        st.toast(f"User Input Submitted - {user_prompt}")
+        crew_result = transactions_crew.kickoff(inputs={"input": user_prompt})
+        content = retrieve_crew_content(crew_result)
+        table = retrieve_crew_table(crew_result)
+        table_frame = pd.DataFrame(table)
 
-if form.form_submit_button("Submit"):
-    st.toast(f"User Input Submitted - {user_prompt}")
-    crew_result = transactions_crew.kickoff(inputs={"input": user_prompt})
-    content = retrieve_crew_content(crew_result)
-    table = retrieve_crew_table(crew_result)
-    table_frame = pd.DataFrame(table)
-
-    messages.chat_message("user").write(user_prompt)
-    st.dataframe(table_frame, use_container_width=True)
-    # for key, value in content.items():
-    # # Display the key as the title
-    #     st.header(key)
-
-    # # Check if the value is a dictionary (like "Price Range")
-    # if isinstance(value, dict):
-    #     # Iterate through the dictionary to display its key-value pairs
-    #     for sub_key, sub_value in value.items():
-    #         st.write(f"**{sub_key}**: {sub_value}")
-    # else:
-    #     # Display the content for other keys
-    #     st.write(value)
-    messages.chat_message("assistant").write(f"HDB Expert: {content}")
-    st.session_state.form_enabled = True
+        messages.chat_message("user").write(user_prompt)
+        st.dataframe(table_frame, use_container_width=True)
+        messages.chat_message("assistant").write(f"HDB Expert: {content}")
+        st.session_state.form_enabled = True
