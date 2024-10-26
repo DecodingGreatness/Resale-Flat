8c461cab821d15f51ff9d9b9864157795184280b
Edward Ho <pythoned77@gmail.com>
2024-10-26 20:50:42 +0800

    print crew result output

--- pages/resale_transaction_price.py
+++ pages/resale_transaction_price.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import sys
 import os
+import json
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 from business_logic.transaction_price.crew import transactions_crew
 import pandas as pd
@@ -19,8 +20,17 @@ def resale_price():
     if submitted_price:
         with st.status("Processing Input...", expanded=True) as status:
             st.toast(f"User Input Submitted - {user_prompt}")
-            crew_result = transactions_crew.kickoff(inputs={"input": user_prompt})
-            print("what is crew result:",crew_result)
+            input_array = {"input": user_prompt}
+            crew_result = transactions_crew.kickoff(inputs=input_array)
+
+            print(f"Raw Output: {crew_result.raw}")
+            if crew_result.json_dict:
+                print(f"JSON Output: {json.dumps(crew_result.json_dict, indent=2)}")
+            if crew_result.pydantic:
+                print(f"Pydantic Output: {crew_result.pydantic}")
+                print(f"Tasks Output: {crew_result.tasks_output}")
+                print(f"Token Usage: {crew_result.token_usage}")
+                print("what is crew result:",crew_result)
             content = retrieve_crew_content(crew_result)
             table = retrieve_crew_table(crew_result)
             table_frame = pd.DataFrame(table)
